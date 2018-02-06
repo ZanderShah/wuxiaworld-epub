a65926aa76b1ff853c892e07805f5fb747750ba3
ZanderShah <alexander.shah@uwaterloo.ca>
2018-02-05 22:35:00 -0500

    in the middle of rewriting to take command line arguments

--- wxw.py
+++ wxw.py
@@ -3,52 +3,73 @@ from bs4 import BeautifulSoup
 from ebooklib import epub
 import uuid
 import base64
+import argparse
 
-book = epub.EpubBook()
+BASE_LINK = 'http://www.wuxiaworld.com/'
 
-book.set_identifier(uuid.uuid4().hex)
-book.set_title('Desolate Era - Book 3 - Comprehending The Way by the Pond')
-book.set_language('en')
-book.add_author('IET')
+def wxw():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-u', '--url', default='desolate-era-index')
+    ap.add_argument('-b', '--books', nargs='+', default=[1])
+    args = ap.parse_args()
 
-# TODO - Generalize for all book / chapter of given title
-base_link = 'http://www.wuxiaworld.com/desolate-era-index/de-book-3-chapter-{}'
-chapters = []
+    index_link = BASE_LINK + args.url
+    index_req = Request(index_link, headers={'User-Agent': 'Mozilla/5.0'})
+    index_soup = BeautifulSoup(urlopen(index_req).read(), 'html5lib')
 
-for i in range(1, 19):
-    chapter_link = base_link.format(i)
-    req = Request(chapter_link, headers={'User-Agent': 'Mozilla/5.0'})
-    page = urlopen(req).read()
-    soup = BeautifulSoup(page, 'html.parser')
+    title = index_soup.find('h1', attrs={'class': 'entry-title'}).get_text().replace(' – Index', '')
+    raw_chapter_links = [a['href'] for a in index_soup.select('div[itemprop=articleBody] a[href]')]
 
-    raw_chapter = soup.find('div', attrs={'itemprop': 'articleBody'})
-    parsed_chapter = []
+    print(raw_chapter_links)
+    return
 
-    hr = 0
-    for line in raw_chapter:
-        if line.name == 'hr':
-            hr += 1
-        elif hr == 1 and line.name == 'p':
-            parsed_chapter.append(line.get_text())
+    book = epub.EpubBook()
+    book.set_identifier(uuid.uuid4().hex)
+    book.set_title(title)
+    book.set_language('en')
 
-    chapter_title = parsed_chapter[0]
-    chapter = epub.EpubHtml(title=chapter_title, file_name='{}.xhtml'.format(chapter_title), lang='en')
-    # Chapter Title
-    parsed_chapter[0] = '<h1>{}</h1>'.format(parsed_chapter[0])
-    chapter.content = '<br /><br />'.join(str(line) for line in parsed_chapter)
+    # TODO - Generalize for all book / chapter of given title
+    base_link = 'http://www.wuxiaworld.com/desolate-era-index/de-book-3-chapter-{}'
 
-    book.add_item(chapter)
-    book.toc += (epub.Link(chapter.file_name, chapter.title, chapter.title), )
-    chapters.append(chapter)
+    chapters = []
 
-book.add_item(epub.EpubNcx())
-book.add_item(epub.EpubNav())
+    for i in range(1, 19):
+        chapter_link = base_link.format(i)
+        req = Request(chapter_link, headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(req).read()
+        soup = BeautifulSoup(page, 'html.parser')
 
-book.spine = ['Nav'] + chapters
+        raw_chapter = soup.find('div', attrs={'itemprop': 'articleBody'})
+        parsed_chapter = []
 
-# Not sure exactly what this is doing
-style = 'BODY {color: white;}'
-nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
-book.add_item(nav_css)
+        hr = 0
+        for line in raw_chapter:
+            if line.name == 'hr':
+                hr += 1
+            elif hr == 1 and line.name == 'p':
+                parsed_chapter.append(line.get_text())
 
-epub.write_epub('{}.epub'.format(book.title), book, {})
+        chapter_title = parsed_chapter[0]
+        chapter = epub.EpubHtml(title=chapter_title, file_name='{}.xhtml'.format(chapter_title), lang='en')
+        # Chapter Title
+        parsed_chapter[0] = '<h1>{}</h1>'.format(parsed_chapter[0])
+        chapter.content = '<br /><br />'.join(str(line) for line in parsed_chapter)
+
+        book.add_item(chapter)
+        book.toc += (epub.Link(chapter.file_name, chapter.title, chapter.title), )
+        chapters.append(chapter)
+
+    book.add_item(epub.EpubNcx())
+    book.add_item(epub.EpubNav())
+
+    book.spine = ['Nav'] + chapters
+
+    # Not sure exactly what this is doing
+    style = 'BODY {color: white;}'
+    nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+    book.add_item(nav_css)
+
+    epub.write_epub('{}.epub'.format(book.title), book, {})
+
+if __name__ == '__main__':
+    wxw()
